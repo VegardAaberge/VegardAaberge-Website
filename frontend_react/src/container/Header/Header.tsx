5fe13d6a16cc525ae3a20886eb1313738cf9a7b1
VegardAaberge <vegard.aaberge@gmail.com>
2022-07-08 19:28:15 +0800

    Remove redundant code

--- frontend_react/src/container/Header/Header.tsx
+++ frontend_react/src/container/Header/Header.tsx
@@ -1,26 +1,11 @@
 import React from "react";
-import { motion } from "framer-motion";
 
-import { images } from "../../constants";
 import "./Header.scss";
 import HeaderInfo from "./HeaderInfo/HeaderInfo";
 import HeaderImg from "./HeaderImg/HeaderImg";
 import HeaderCircles from "./HeaderCircles/HeaderCircles";
 
-const scaleVariants = {
-  whileInView: {
-    scale: [0, 1],
-    opacity: [0, 1],
-    transition: {
-      duration: 1,
-      ease: "easeInOut",
-    },
-  },
-};
-
-interface Props {}
-
-const Header: React.FC<Props> = () => {
+const Header: React.FC = () => {
   return (
     <div id="home" className="app__header app__flex-center">
       <HeaderInfo />
