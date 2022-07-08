876c7c2a63b94d441eff9828d6dba9536e8b9509
VegardAaberge <vegard.aaberge@gmail.com>
2022-07-08 19:27:49 +0800

    Remove redundant code

--- frontend_react/src/container/About/About.tsx
+++ frontend_react/src/container/About/About.tsx
@@ -1,13 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+import { images } from "../../constants";
 import "./About.scss";
 
-interface Props {}
+interface AboutItem {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
 
-const About: React.FC<Props> = () => {
+const abouts: AboutItem[] = [
+  {
+    title: "Android Development",
+    description: "I am a good web developer",
+    imageUrl: images.about01,
+  },
+  {
+    title: "Xamarin",
+    description: "I am a good web developer",
+    imageUrl: images.about02,
+  },
+  {
+    title: "Spring Boot",
+    description: "I am a good web developer",
+    imageUrl: images.about03,
+  },
+  {
+    title: "React",
+    description: "I am a good web developer",
+    imageUrl: images.about04,
+  },
+];
+
+const About: React.FC = () => {
   return (
-    <div>
-      <div>About</div>
-    </div>
+    <>
+      <h2 className="head-text">
+        I know that
+        <span>Good Design</span>
+        <br />
+        means
+        <span>Good business</span>
+      </h2>
+
+      <div className="app_profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app_profiles-item"
+            key={about.title + index}
+          >
+            <img src={about.imageUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 };
 
