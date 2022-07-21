6c44ca15f586ddb2ed51ae4d5cc82081427ba8a4
VegardAaberge <vegard.aaberge@gmail.com>
2022-07-21 14:20:12 +0800

    Improve the appearance of Home on tablets and desktops

--- frontend_react/src/container/Home/HomeInfo/HomeInfo.tsx
+++ frontend_react/src/container/Home/HomeInfo/HomeInfo.tsx
@@ -9,20 +9,16 @@ const HomeInfo: React.FC = () => {
       <h1 className="app__home-info-name">Vegard Aaberge</h1>
       <div className="app__home-info-title">
         <div className="app__home-info-title-wrapper">
-          {[
-            "App Developer",
-            "Web Developer",
-            "Photographer",
-            "Writer",
-            "Content Creator",
-          ].map((item) => (
-            <div className="app__home-info-title-item">{item}</div>
-          ))}
+          {["Xamarin", "Native Android", "UI/UX", "React", "Spring Boot"].map(
+            (item) => (
+              <div className="app__home-info-title-item">{item}</div>
+            )
+          )}
         </div>
       </div>
       <p className="app__home-info-desc">
-        I design and develop services for customers of all sizes, specializing
-        in creating stylish, modern websites, web services and online stores.
+        I have severall years of experience developing mobile apps and web
+        applications for both small and large customers.
       </p>
     </div>
   );
