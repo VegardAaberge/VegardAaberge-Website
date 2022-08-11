82bc0037592930c2bc246df34d4b53e83269e91b
VegardAaberge <vegard.aaberge@gmail.com>
2022-08-11 16:14:04 +0800

    Adding nprogress to handle website loading.

--- frontend_react/src/pages/_app.tsx
+++ frontend_react/src/pages/_app.tsx
@@ -1,10 +1,35 @@
+import React, { useEffect } from "react";
+import Router, { useRouter } from "next/router";
+import NProgress from "nprogress";
+
 import "../styles/globals.css";
 import "../styles/workGlobals.css";
+import "nprogress/nprogress.css";
 
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
